b952157517617c2fa473f69fb0c10d14d44a5c37
egor_yanukovich <egoryanukovich2@gmail.com>
2024-08-19 16:12:11 +0200

    created project

---
+++ NewsTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTestApp
+//
+//  Created by Egor Yanukovich on 19.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
